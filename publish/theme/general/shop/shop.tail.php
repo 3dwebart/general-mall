41c5193769f18218ab14cbfd32354cbdd413d20d
3dwebart <3d.web.artist@gmail.com>
2019-06-03 16:50:19 +0900

    2019-06-01 : Channge Language english

--- publish/theme/general/shop/shop.tail.php
+++ publish/theme/general/shop/shop.tail.php
@@ -22,38 +22,49 @@ if(!defined('_INDEX_')):
 <!-- 하단 시작 { -->
 <div id="ft">
 	<div class="ft_wr">
-		<ul class="ft_ul">
-			<li><a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=company">About Us</a></li>
-			<li><a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=provision">Terms of Service</a></li>
-			<li><a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=privacy">Privacy Statement</a></li>
-			<li><a href="<?php echo get_device_change_url(); ?>">Mobile version</a></li>
-		</ul>
-		
-		<a href="<?php echo G5_SHOP_URL; ?>/" id="ft_logo"><img src="<?php echo G5_DATA_URL; ?>/common/logo_img2" alt="처음으로"></a>
-
-		<div class="ft_info">
-			<span class="d-block"><b>Company name</b> <?php echo $default['de_admin_company_name']; ?></span>
-			<span class="d-block"><b>Business address</b> <?php echo $default['de_admin_company_addr']; ?></span>
-			<span class="d-block">
-				<span><b>Business number</b> <?php echo $default['de_admin_company_saupja_no']; ?></span>
-				<span><b>CEO</b> <?php echo $default['de_admin_company_owner']; ?></span>
-			</span>
-			<span class="d-block">
-				<span><b>Tel</b> <?php echo $default['de_admin_company_tel']; ?></span>
-				<span><b>Fax</b> <?php echo $default['de_admin_company_fax']; ?></span>
-			</span>
-			<!-- <span><b>운영자</b> <?php echo $admin['mb_name']; ?></span><br> -->
-			<span class="d-block"><b>Mail order sales report number</b> <?php echo $default['de_admin_tongsin_no']; ?></span>
-			<span class="d-block"><b>Privacy Officer</b> <?php echo $default['de_admin_info_name']; ?></span>
-
-			<?php if ($default['de_admin_buga_no']) echo '<span><b>부가통신사업신고번호</b> '.$default['de_admin_buga_no'].'</span>'; ?><br>
-			Copyright &copy; 2001-2013 <?php echo $default['de_admin_company_name']; ?>. All Rights Reserved.
+		<div class="ft-nav">
+			<div class="container">
+				<ul class="ft_ul">
+					<li><a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=company">About Us</a></li>
+					<li><a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=provision">Terms of Service</a></li>
+					<li><a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=privacy">Privacy Statement</a></li>
+					<li><a href="<?php echo get_device_change_url(); ?>">Mobile version</a></li>
+				</ul>
+			</div>
 		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<a href="<?php echo G5_SHOP_URL; ?>/" id="ft_logo"><img src="<?php echo G5_DATA_URL; ?>/common/logo_img2" alt="처음으로" class="img-fluid"></a>
+				</div>
+				<div class="col-lg-6">
+					<div class="ft_info">
+						<span class="d-block"><b>Company name</b> <?php echo $default['de_admin_company_name']; ?></span>
+						<span class="d-block"><b>Business address</b> <?php echo $default['de_admin_company_addr']; ?></span>
+						<span class="d-block">
+							<span><b>Business number</b> <?php echo $default['de_admin_company_saupja_no']; ?></span>
+							<span><b>CEO</b> <?php echo $default['de_admin_company_owner']; ?></span>
+						</span>
+						<span class="d-block">
+							<span><b>Tel</b> <?php echo $default['de_admin_company_tel']; ?></span>
+							<span><b>Fax</b> <?php echo $default['de_admin_company_fax']; ?></span>
+						</span>
+						<!-- <span><b>운영자</b> <?php echo $admin['mb_name']; ?></span><br> -->
+						<span class="d-block"><b>Mail order sales report number</b> <?php echo $default['de_admin_tongsin_no']; ?></span>
+						<span class="d-block"><b>Privacy Officer</b> <?php echo $default['de_admin_info_name']; ?></span>
 
-		<div class="ft_cs">
-			<h2>Customer center</h2>
-			<strong>02-123-1234</strong>
-			<p><b>Mon-Fri</b> Korea time : am 09:00 - pm 05:00<br>Lunch hour : am 12:00 - pm 01:00</p>
+						<?php if ($default['de_admin_buga_no']) echo '<span><b>부가통신사업신고번호</b> '.$default['de_admin_buga_no'].'</span>'; ?><br>
+						Copyright &copy; 2001-2013 <?php echo $default['de_admin_company_name']; ?>. All Rights Reserved.
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="ft_cs">
+						<h2>Customer center</h2>
+						<strong>032-581-3466</strong>
+						<p><b>Mon-Fri</b> Korea time : am 09:00 - pm 05:00<br>Lunch hour : am 12:00 - pm 01:00</p>
+					</div>
+				</div>
+			</div>
 		</div>
 		<button type="button" id="top_btn"><i class="fa fa-arrow-up" aria-hidden="true"></i><span class="sound_only">Top</span></button>
 		<script>
