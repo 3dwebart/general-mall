90bdc11303cdcc991bdad15c43fd035e0f00e146
3dwebart <3d.web.artist@gmail.com>
2019-07-05 17:52:07 +0900

    2019-07-05 : social login / code cleanup

--- publish/skin/social/social_login.skin.php
+++ publish/skin/social/social_login.skin.php
@@ -18,42 +18,42 @@ add_stylesheet('<link rel="stylesheet" href="'.get_social_skin_url().'/style.css
 ?>
 
 <div id="sns_login" class="login-sns sns-wrap-32 sns-wrap-over">
-    <h3>소셜계정으로 로그인</h3>
+    <h3>Social login</h3>
     <div class="sns-wrap">
         <?php if( social_service_check('naver') ) {     //네이버 로그인을 사용한다면 ?>
         <a href="<?php echo $self_url;?>?provider=naver&amp;url=<?php echo $urlencode;?>" class="sns-icon social_link sns-naver" title="네이버">
             <span class="ico"></span>
-            <span class="txt">네이버<i> 로그인</i></span>
+            <span class="txt">Naver<i> sign in</i></span>
         </a>
         <?php }     //end if ?>
         <?php if( social_service_check('kakao') ) {     //카카오 로그인을 사용한다면 ?>
         <a href="<?php echo $self_url;?>?provider=kakao&amp;url=<?php echo $urlencode;?>" class="sns-icon social_link sns-kakao" title="카카오">
             <span class="ico"></span>
-            <span class="txt">카카오<i> 로그인</i></span>
+            <span class="txt">kakao<i> sign in</i></span>
         </a>
         <?php }     //end if ?>
         <?php if( social_service_check('facebook') ) {     //페이스북 로그인을 사용한다면 ?>
         <a href="<?php echo $self_url;?>?provider=facebook&amp;url=<?php echo $urlencode;?>" class="sns-icon social_link sns-facebook" title="페이스북">
             <span class="ico"></span>
-            <span class="txt">페이스북<i> 로그인</i></span>
+            <span class="txt">Facebook<i> sign in</i></span>
         </a>
         <?php }     //end if ?>
         <?php if( social_service_check('google') ) {     //구글 로그인을 사용한다면 ?>
         <a href="<?php echo $self_url;?>?provider=google&amp;url=<?php echo $urlencode;?>" class="sns-icon social_link sns-google" title="구글">
             <span class="ico"></span>
-            <span class="txt">구글+<i> 로그인</i></span>
+            <span class="txt">Google<i> sign in</i></span>
         </a>
         <?php }     //end if ?>
         <?php if( social_service_check('twitter') ) {     //트위터 로그인을 사용한다면 ?>
         <a href="<?php echo $self_url;?>?provider=twitter&amp;url=<?php echo $urlencode;?>" class="sns-icon social_link sns-twitter" title="트위터">
             <span class="ico"></span>
-            <span class="txt">트위터<i> 로그인</i></span>
+            <span class="txt">Twitter<i> sign in</i></span>
         </a>
         <?php }     //end if ?>
         <?php if( social_service_check('payco') ) {     //페이코 로그인을 사용한다면 ?>
         <a href="<?php echo $self_url;?>?provider=payco&amp;url=<?php echo $urlencode;?>" class="sns-icon social_link sns-payco" title="페이코">
             <span class="ico"></span>
-            <span class="txt">페이코<i> 로그인</i></span>
+            <span class="txt">Payco<i> sign in</i></span>
         </a>
         <?php }     //end if ?>
 
@@ -80,6 +80,5 @@ add_stylesheet('<link rel="stylesheet" href="'.get_social_skin_url().'/style.css
             });
         </script>
         <?php } ?>
-
     </div>
 </div>
--- publish/theme/general/skin/member/basic/login.skin.php
+++ publish/theme/general/skin/member/basic/login.skin.php
@@ -7,123 +7,121 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 
 <!-- 로그인 시작 { -->
 <div id="mb_login" class="mbskin">
-    <h1><?php echo $g5['title'] ?></h1>
+	<h1><?php echo $g5['title'] ?></h1>
+	<form name="flogin" action="<?php echo $login_action_url ?>" onsubmit="return flogin_submit(this);" method="post">
+		<input type="hidden" name="url" value="<?php echo $login_url ?>">
 
-    <form name="flogin" action="<?php echo $login_action_url ?>" onsubmit="return flogin_submit(this);" method="post">
-    <input type="hidden" name="url" value="<?php echo $login_url ?>">
+		<fieldset id="login_fs">
+			<legend>Member login</legend>
+			<label for="login_id" class="sound_only">ID<strong class="sound_only"> Necessary</strong></label>
+			<input type="text" name="mb_id" id="login_id" required class="frm_input required" size="20" maxLength="20" placeholder="ID">
+			<label for="login_pw" class="sound_only">Password<strong class="sound_only"> Necessary</strong></label>
+			<input type="password" name="mb_password" id="login_pw" required class="frm_input required" size="20" maxLength="20" placeholder="Password">
+			<input type="submit" value="Login" class="btn_submit">
+			<input type="checkbox" name="auto_login" id="login_auto_login">
+			<label for="login_auto_login">Auto login</label>
+		</fieldset>
+		<?php
+		// 소셜로그인 사용시 소셜로그인 버튼
+		@include_once(get_social_skin_path().'/social_login.skin.php');
+		?>
 
-    <fieldset id="login_fs">
-        <legend>Member login</legend>
-        <label for="login_id" class="sound_only">ID<strong class="sound_only"> Necessary</strong></label>
-        <input type="text" name="mb_id" id="login_id" required class="frm_input required" size="20" maxLength="20" placeholder="ID">
-        <label for="login_pw" class="sound_only">Password<strong class="sound_only"> Necessary</strong></label>
-        <input type="password" name="mb_password" id="login_pw" required class="frm_input required" size="20" maxLength="20" placeholder="Password">
-        <input type="submit" value="Login" class="btn_submit">
-        <input type="checkbox" name="auto_login" id="login_auto_login">
-        <label for="login_auto_login">Auto login</label>
-    </fieldset>
+		<aside id="login_info">
+			<h2>Member login info</h2>
+			<div>
+				<a href="<?php echo G5_BBS_URL ?>/password_lost.php" target="_blank" id="login_password_lost">Find ID & password</a>
+				<a href="./register.php">Member sign up</a>
+			</div>
+		</aside>
 
-    <?php
-    // 소셜로그인 사용시 소셜로그인 버튼
-    @include_once(get_social_skin_path().'/social_login.skin.php');
-    ?>
+	</form>
 
-    <aside id="login_info">
-        <h2>Member login info</h2>
-        <div>
-            <a href="<?php echo G5_BBS_URL ?>/password_lost.php" target="_blank" id="login_password_lost">Find ID & password</a>
-            <a href="./register.php">Member sign up</a>
-        </div>
-    </aside>
+	
+	<?php // 쇼핑몰 사용시 여기부터 ?>
+	<?php if ($default['de_level_sell'] == 1) { // 상품구입 권한 ?>
 
-    </form>
+		<!-- 주문하기, 신청하기 -->
+		<?php if (preg_match("/orderform.php/", $url)) { ?>
 
-    
-    <?php // 쇼핑몰 사용시 여기부터 ?>
-    <?php if ($default['de_level_sell'] == 1) { // 상품구입 권한 ?>
+	<section id="mb_login_notmb">
+		<h2>비회원 구매</h2>
 
-        <!-- 주문하기, 신청하기 -->
-        <?php if (preg_match("/orderform.php/", $url)) { ?>
+		<p>
+			비회원으로 주문하시는 경우 포인트는 지급하지 않습니다.
+		</p>
 
-    <section id="mb_login_notmb">
-        <h2>비회원 구매</h2>
+		<div id="guest_privacy">
+			<?php echo $default['de_guest_privacy']; ?>
+		</div>
 
-        <p>
-            비회원으로 주문하시는 경우 포인트는 지급하지 않습니다.
-        </p>
+		<label for="agree">개인정보수집에 대한 내용을 읽었으며 이에 동의합니다.</label>
+		<input type="checkbox" id="agree" value="1">
 
-        <div id="guest_privacy">
-            <?php echo $default['de_guest_privacy']; ?>
-        </div>
+		<div class="btn_confirm">
+			<a href="javascript:guest_submit(document.flogin);" class="btn_submit">비회원으로 구매하기</a>
+		</div>
 
-        <label for="agree">개인정보수집에 대한 내용을 읽었으며 이에 동의합니다.</label>
-        <input type="checkbox" id="agree" value="1">
+		<script>
+		function guest_submit(f)
+		{
+			if (document.getElementById('agree')) {
+				if (!document.getElementById('agree').checked) {
+					alert("개인정보수집에 대한 내용을 읽고 이에 동의하셔야 합니다.");
+					return;
+				}
+			}
 
-        <div class="btn_confirm">
-            <a href="javascript:guest_submit(document.flogin);" class="btn_submit">비회원으로 구매하기</a>
-        </div>
+			f.url.value = "<?php echo $url; ?>";
+			f.action = "<?php echo $url; ?>";
+			f.submit();
+		}
+		</script>
+	</section>
 
-        <script>
-        function guest_submit(f)
-        {
-            if (document.getElementById('agree')) {
-                if (!document.getElementById('agree').checked) {
-                    alert("개인정보수집에 대한 내용을 읽고 이에 동의하셔야 합니다.");
-                    return;
-                }
-            }
+	<?php } else if (preg_match("/orderinquiry.php$/", $url)) { ?>
+	<div id="mb_login_od_wr">
+		<h2>비회원 주문조회 </h2>
 
-            f.url.value = "<?php echo $url; ?>";
-            f.action = "<?php echo $url; ?>";
-            f.submit();
-        }
-        </script>
-    </section>
+		<fieldset id="mb_login_od">
+			<legend>비회원 주문조회</legend>
 
-    <?php } else if (preg_match("/orderinquiry.php$/", $url)) { ?>
-    <div id="mb_login_od_wr">
-        <h2>비회원 주문조회 </h2>
+			<form name="forderinquiry" method="post" action="<?php echo urldecode($url); ?>" autocomplete="off">
 
-        <fieldset id="mb_login_od">
-            <legend>비회원 주문조회</legend>
+			<label for="od_id" class="od_id sound_only">주문서번호<strong class="sound_only"> 필수</strong></label>
+			<input type="text" name="od_id" value="<?php echo $od_id; ?>" id="od_id" required class="frm_input required" size="20" placeholder="주문서번호">
+			<label for="id_pwd" class="od_pwd sound_only" >비밀번호<strong class="sound_only"> 필수</strong></label>
+			<input type="password" name="od_pwd" size="20" id="od_pwd" required class="frm_input required" placeholder="비밀번호">
+			<input type="submit" value="확인" class="btn_submit">
 
-            <form name="forderinquiry" method="post" action="<?php echo urldecode($url); ?>" autocomplete="off">
+			</form>
+		</fieldset>
 
-            <label for="od_id" class="od_id sound_only">주문서번호<strong class="sound_only"> 필수</strong></label>
-            <input type="text" name="od_id" value="<?php echo $od_id; ?>" id="od_id" required class="frm_input required" size="20" placeholder="주문서번호">
-            <label for="id_pwd" class="od_pwd sound_only" >비밀번호<strong class="sound_only"> 필수</strong></label>
-            <input type="password" name="od_pwd" size="20" id="od_pwd" required class="frm_input required" placeholder="비밀번호">
-            <input type="submit" value="확인" class="btn_submit">
+		<section id="mb_login_odinfo">
+			<p>메일로 발송해드린 주문서의 <strong>주문번호</strong> 및 주문 시 입력하신 <strong>비밀번호</strong>를 정확히 입력해주십시오.</p>
+		</section>
 
-            </form>
-        </fieldset>
+	</div>
+	<?php } ?>
 
-        <section id="mb_login_odinfo">
-            <p>메일로 발송해드린 주문서의 <strong>주문번호</strong> 및 주문 시 입력하신 <strong>비밀번호</strong>를 정확히 입력해주십시오.</p>
-        </section>
+	<?php } ?>
+	<?php // 쇼핑몰 사용시 여기까지 반드시 복사해 넣으세요 ?>
 
-    </div>
-    <?php } ?>
-
-    <?php } ?>
-    <?php // 쇼핑몰 사용시 여기까지 반드시 복사해 넣으세요 ?>
-
-    </form>
+	</form>
 
 </div>
 
 <script>
 $(function(){
-    $("#login_auto_login").click(function(){
-        if (this.checked) {
-            this.checked = confirm("자동로그인을 사용하시면 다음부터 회원아이디와 비밀번호를 입력하실 필요가 없습니다.\n\n공공장소에서는 개인정보가 유출될 수 있으니 사용을 자제하여 주십시오.\n\n자동로그인을 사용하시겠습니까?");
-        }
-    });
+	$("#login_auto_login").click(function(){
+		if (this.checked) {
+			this.checked = confirm("자동로그인을 사용하시면 다음부터 회원아이디와 비밀번호를 입력하실 필요가 없습니다.\n\n공공장소에서는 개인정보가 유출될 수 있으니 사용을 자제하여 주십시오.\n\n자동로그인을 사용하시겠습니까?");
+		}
+	});
 });
 
 function flogin_submit(f)
 {
-    return true;
+	return true;
 }
 </script>
 <!-- } 로그인 끝 -->
