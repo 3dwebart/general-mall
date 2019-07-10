9c6b2f4d9d50c6db15f022e9437144db3657a445
3dwebart <3d.web.artist@gmail.com>
2019-07-10 09:49:18 +0900

    2019-07-10 : Code cleanup

--- g5-uninstall-ok.php
+++ g5-uninstall-ok.php
@@ -7,224 +7,224 @@ $dbName      = $_POST['dbName'];
 $con=mysqli_connect($host,$userName,$password,$dbName);
 // Check connection
 if (mysqli_connect_errno()) {
-    die("Failed to connect to MySQL: " . mysqli_connect_error());
+	die("Failed to connect to MySQL: " . mysqli_connect_error());
 }
 /*
 // DROP TABLE
 $sql = "DROP TABLE IF EXISTS `g5_auth`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_autosave`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_board_new`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_board_file`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_board_good`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_board`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_cert_history`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_config`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_content`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_faq_master`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_faq`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_group_member`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_group`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_login`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_mail`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_member_social_profiles`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_member`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_memo`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_menu`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_new_win`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_point`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_poll_etc`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_poll`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_popular`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_qa_content`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_qa_config`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_scrap`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_banner`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_category`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_cart`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_coupon_zone`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_coupon_log`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_coupon`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_default`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_event_item`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_event`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_inicis_log`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_item_option`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_item_relation`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_item_stocksms`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_item_use`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_item_qa`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 $sql = "DROP TABLE IF EXISTS `g5_shop_item`";
 $result = mysqli_query($con,$sql)
 if (!$result) {
-    die("Error creating database: " . mysqli_error($con));
+	die("Error creating database: " . mysqli_error($con));
 }
 
 
@@ -245,7 +245,7 @@ g5_write_gallery
 g5_write_notice
 g5_write_qa
 */
-$sql = new mysqli($host,$userName,$password,$dbName);
-
+$mysqli = new sql($host,$userName,$password,$dbName);
+print_r($mysqli);
 mysqli_close($con);
 ?>
---
+++ g5-uninstall.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>YC5 Uninstaller</title>
+</head>
+<body>
+<form action="g5-uninstall-ok.php" method="post" enctype="multipart/form-data">
+	<label for="host">Host</label>
+	<input type="text" name="host" id="host">
+	<label for="userName">User name</label>
+	<input type="text" name="userName" id="userName">
+	<label for="password">Password</label>
+	<input type="text" name="password" id="password">
+	<label for="dbName">DB Name</label>
+	<input type="text" name="dbName" id="dbName">
+</form>
+</body>
+</html>
