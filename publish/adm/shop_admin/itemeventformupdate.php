0f659cfaffbe68098b73e68a80d1554e808dcd06
3dwebart <3d.web.artist@gmail.com>
2019-07-15 13:34:36 +0900

    2019-07-15 : KVE-2019-0829 영카트 XSS 취약점 수정

--- publish/adm/shop_admin/itemeventformupdate.php
+++ publish/adm/shop_admin/itemeventformupdate.php
@@ -25,7 +25,8 @@ $ev_mobile_skin = preg_replace('#\.+(\/|\\\)#', '', $ev_mobile_skin);
 $skin_regex_patten = "^list.[0-9]+\.skin\.php";
 
 $ev_skin = (preg_match("/$skin_regex_patten/", $ev_skin) && file_exists(G5_SHOP_SKIN_PATH.'/'.$ev_skin)) ? $ev_skin : ''; 
-$ev_mobile_skin = (preg_match("/$skin_regex_patten/", $ev_mobile_skin) && file_exists(G5_MSHOP_SKIN_PATH.'/'.$ev_mobile_skin)) ? $ev_mobile_skin : ''; 
+$ev_mobile_skin = (preg_match("/$skin_regex_patten/", $ev_mobile_skin) && file_exists(G5_MSHOP_SKIN_PATH.'/'.$ev_mobile_skin)) ? $ev_mobile_skin : '';
+$ev_subject = strip_tags($ev_subject);
 $ev_kind = $_POST['ev_kind'];
 $co_id   = $_POST['co_id'];
 
