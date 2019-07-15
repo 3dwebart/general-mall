360f092278e8697c047186bc2c55af22e5dea39e
3dwebart <3d.web.artist@gmail.com>
2019-07-15 15:41:18 +0900

    2019-07-15 : 잘못 패치된 코드 수정

--- publish/adm/shop_admin/categorylistupdate.php
+++ publish/adm/shop_admin/categorylistupdate.php
@@ -58,8 +58,10 @@ for ($i=0; $i<count($_POST['ca_id']); $i++)
         }
     }
 
+    $p_ca_name = is_array($_POST['ca_name']) ? strip_tags($_POST['ca_name'][$i]) : '';
+
     $sql = " update {$g5['g5_shop_category_table']}
-                set ca_name             = '".sql_real_escape_string(strip_tags($_POST['ca_name'][$i]))."',
+                set ca_name             = '".$p_ca_name."',
                     ca_mb_id            = '".sql_real_escape_string(strip_tags($_POST['ca_mb_id'][$i]))."',
                     ca_use              = '".sql_real_escape_string(strip_tags($_POST['ca_use'][$i]))."',
                     ca_list_mod         = '".sql_real_escape_string(strip_tags($_POST['ca_list_mod'][$i]))."',
